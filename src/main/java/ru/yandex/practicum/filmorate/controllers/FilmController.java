45a04397077a2c12867a3aaf96ee31292f0c37aa
PVD <cheaterpvd@yandex.ru>
2023-03-26 22:02:48 +0300

    checkStyle fix

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -30,32 +30,32 @@ public class FilmController {
     }
 
     @PostMapping()
-    public Film addFilm (@RequestBody @Valid Film film) {
+    public Film addFilm(@RequestBody @Valid Film film) {
         return filmStorage.addFilm(film);
     }
 
     @PutMapping()
-    public Film updateFilm (@RequestBody @Valid Film film) {
+    public Film updateFilm(@RequestBody @Valid Film film) {
         return filmStorage.updateFilm(film);
     }
 
     @PutMapping("/{id}/like/{userId}")
-    public Film putLikeToFilm (@PathVariable int id, @PathVariable int userId) {
+    public Film putLikeToFilm(@PathVariable int id, @PathVariable int userId) {
         return filmService.putLikeToFilm(id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
-    public Film deleteLikeFromFilm (@PathVariable int id, @PathVariable int userId) {
+    public Film deleteLikeFromFilm(@PathVariable int id, @PathVariable int userId) {
         return filmService.deleteLikeFromFilm(id, userId);
     }
     @GetMapping("/popular")
-    public List<Film> getPopularFilms (@RequestParam(value = "count",
-            required = false, defaultValue = "10") @Positive int count) {
+    public List<Film> getPopularFilms(@RequestParam(value = "count"
+            ,required = false, defaultValue = "10") @Positive int count) {
         return filmService.getPopularFilm(count);
     }
 
     @GetMapping("{id}")
-    public Film getFilm (@PathVariable int id) {
+    public Film getFilm(@PathVariable int id) {
         Film film = filmStorage.getFilm(id);
         if (film == null) {
             throw new FilmNotFoundException("Запрошенный фильм не существует");
