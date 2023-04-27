91be8e0a8f69fe8a511c213421bb9ad992bd1825
Terekhoff Sasha <terekhoff82@mail.ru>
2023-04-28 01:33:51 +0400

    fix: code style.

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
@@ -19,6 +19,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class DirectorDbStorage implements DirectorStorage {
     private final JdbcTemplate jdbcTemplate;
+
     @Override
     public List<Director> getAll() {
         String sql = "select * from director";
